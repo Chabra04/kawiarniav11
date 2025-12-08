c39b1f000ffc05bac806bf420a13162720a5e075
Chabra04 <stachniukbartek@gmail.com>
2025-12-08 11:23:45 +0100

    Enhance Gameplay class with score and level management
    Added functionality for scoring and level progression.

--- Gameplay.java
+++ Gameplay.java
@@ -1,18 +1,68 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 public class Gameplay {
-    private Level level;
 
-    public Gameplay(){
-        level=new Level(1,3000);
+    private Level level;
+    private int currentLevel;
+    private int score;
+
+    private BufferedImage background; // tło gry
+
+    public Gameplay(int currentLevel, int score) {
+        this.currentLevel = currentLevel;
+        this.score = score;
+
+        // wczytanie tła gry z folderu resources (poprawna metoda w Javie)
+        try {
+            background = ImageIO.read(getClass().getResourceAsStream("/resources/tloGry.jpg"));
+        } catch (Exception e) {
+            System.out.println("Nie udało się wczytać tła gry!");
+            background = null;
+            e.printStackTrace();
+        }
+
+        level = new Level(currentLevel, 3000, this);
         level.start();
     }
 
     public void update() {
-    level.update();
+        level.update();
     }
 
     public void render(Graphics g) {
+        // rysowanie tła
+        if (background != null) {
+            g.drawImage(background, 0, 0, Game.WIDTH, Game.HEIGHT, null);
+        } else {
+            g.setColor(new Color(230, 220, 200));
+            g.fillRect(0, 0, Game.WIDTH, Game.HEIGHT);
+        }
+
+        // rysowanie poziomu
         level.render(g);
+    }
+
+    public void addScore(int points) {
+        score += points;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void nextLevel() {
+        currentLevel++;
+        level = new Level(currentLevel, 3000, this);
+        level.start();
+    }
+
+    public void mouseClicked(int mx, int my) {
+        level.click(mx, my);
+    }
+
+    public Level getLevel() {
+        return level;
     }
 }
