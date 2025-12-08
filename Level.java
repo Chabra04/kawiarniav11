bb1b5e747cec596b26c38ebb171b45992a55ab5c
Chabra04 <stachniukbartek@gmail.com>
2025-12-08 11:23:22 +0100

    Refactor Level class to include acceptedOrders and gameplay

--- Level.java
+++ Level.java
@@ -11,88 +11,143 @@ public class Level {
     private boolean started;
     private boolean finished;
 
-    private List<Client>clients;
-    private Random  random= new Random();
+    private List<Client> clients;
+    private List<String> acceptedOrders;
+    private Random random = new Random();
 
-    private String[] names={
-            "Anna", "Piotr", "Julia", "Marek", "Krzysztof",
-            "Natalia", "Kinga", "Oskar", "Bartek", "Zuzanna"
-    };
+    private Gameplay gameplay;
 
-    private String[] drinks ={
-            "Latte", "Espresso", "Cappuccino", "Americano", "Mocha"
-    };
+    private String[] names = {"Anna", "Piotr", "Julia", "Marek", "Krzysztof",
+            "Natalia", "Kinga", "Oskar", "Bartek", "Zuzanna"};
 
-    public Level(int number, int timeLimit){
+    private String[] drinks = {"Latte", "Espresso", "Cappuccino", "Americano", "Mocha"};
+
+    public Level(int number, int timeLimit, Gameplay gameplay) {
         this.levelNumber = number;
         this.timeLimit = timeLimit;
-        this.timeLeft=timeLimit;
+        this.timeLeft = timeLimit;
+        this.gameplay = gameplay;
 
         this.started = false;
         this.finished = false;
 
         clients = new ArrayList<>();
+        acceptedOrders = new ArrayList<>();
     }
 
-    //start i koniec poziomu
-    public void start(){
-        started=true;
+    public void start() {
+        started = true;
         finished = false;
-        timeLeft= timeLimit;
-
-        generateClients(3);
-    }
-    public void finish(){
-        finished=true;
-        started=false;
+        timeLeft = timeLimit;
+        generateClients(3); // generujemy 3 klientów
     }
 
-    private void generateClients(int count){
+    private void generateClients(int count) {
         clients.clear();
 
-        for (int i=0;i<count; i++)  {
-            String randomName=names[random.nextInt(names.length)];
+        int screenWidth = 800; // tutaj można dynamicznie pobrać szerokość okna
+        int startX = screenWidth / 2 - 60; // wyśrodkowanie klientów (szerokość klienta = 120)
+        int startY = 200;
+        int gap = 180;
+
+        for (int i = 0; i < count; i++) {
+            String randomName = names[random.nextInt(names.length)];
             String randomDrink = drinks[random.nextInt(drinks.length)];
-
-            clients.add(new Client(randomName, randomDrink));
+            clients.add(new Client(randomName, randomDrink, startX, startY + i * gap));
         }
     }
 
-    //Update :D
-    public void update(){
-        if(!started ||finished) return;
+    public void update() {
+        if (!started || finished) return;
 
-        if(timeLeft>0){
-            timeLeft--;
-        } else {finish();}
+        if (timeLeft > 0) timeLeft--;
+        else finish();
+    }
 
-        for(Client c : clients){
-            c.update();
+    // obsługa kliknięcia myszy
+    public void click(int mx, int my) {
+        for (Client c : clients) {
+            if (c.contains(mx, my)) {
+                c.showOrder();
+            }
+            if (c.isAcceptButton(mx, my)) {
+                accept(c);
+            }
+            if (c.isRejectButton(mx, my)) {
+                reject(c);
+            }
         }
     }
 
-    //render
+    private void accept(Client c) {
+        if (!c.isProcessed()) {
+            c.acceptOrder();
+            acceptedOrders.add(c.getOrder());
+            gameplay.addScore(10);
+            checkLevelEnd();
+        }
+    }
 
-    public void render(Graphics g){
+    private void reject(Client c) {
+        if (!c.isProcessed()) {
+            c.rejectOrder();
+            gameplay.addScore(-5);
+            checkLevelEnd();
+        }
+    }
+
+    private void checkLevelEnd() {
+        boolean allProcessed = true;
+        for (Client c : clients) {
+            if (!c.isProcessed()) {
+                allProcessed = false;
+                break;
+            }
+        }
+        if (allProcessed) finish();
+    }
+
+    private void finish() {
+        finished = true;
+        started = false;
+        SaveManager.save(levelNumber, gameplay.getScore());
+    }
+
+    public void render(Graphics g) {
         g.setColor(Color.BLACK);
-        g.setFont(new Font("Arial",Font.BOLD,50));
-        g.drawString("poziom "+ levelNumber,200,150);
+        g.setFont(new Font("Arial", Font.BOLD, 50));
+        g.drawString("Poziom " + levelNumber, 50, 50);
 
-        g.setFont(new Font("Arial",Font.PLAIN,30));
-        g.drawString("pozostaly czas: "+ timeLeft,200,200);
+        g.setFont(new Font("Arial", Font.PLAIN, 30));
+        g.drawString("Pozostały czas: " + timeLeft, 50, 100);
+        g.drawString("Punkty: " + gameplay.getScore(), 50, 150);
 
-        int x=600;
-        int y= 150;
-        for (Client c:clients){
-            c.render(g,x,y);
-            y+=180;
+        // rysowanie klientów
+        for (Client c : clients) {
+            c.render(g);
         }
 
-        if(finished){
-            g.setColor((Color.RED));
-            g.setFont(new Font("Arial",Font.BOLD,40));
-            g.drawString("koniec poziomu",200,320);
-
+        // lista zaakceptowanych zamówień po prawej
+        g.setFont(new Font("Arial", Font.PLAIN, 18));
+        g.drawString("Zamówienia zaakceptowane:", 500, 50);
+        int y = 80;
+        for (String order : acceptedOrders) {
+            g.drawString(order, 500, y);
+            y += 25;
         }
+
+        if (finished) {
+            g.setColor(Color.RED);
+            g.setFont(new Font("Arial", Font.BOLD, 40));
+            g.drawString("Koniec poziomu!", 200, 400);
+        }
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public boolean isFinished() {
+        return finished;
     }
 }
